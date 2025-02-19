584634080bf72ee1c0242b2c803b748323a15372
whodat01 <jjoseph11@me.com>
2025-02-18 23:13:06 -0600

    (revised)

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ def main():
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
 
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+    
 def get_num_words(text):
     words = text.split()
     return len(words)
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
 
 main()
 
